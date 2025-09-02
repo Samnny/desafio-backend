87c83a077c4cef531e538b6594165cc34badb398
Samay Pessoa <arolllysamay@gmail.com>
2025-09-02 12:40:48 -0300

    feat(task): implement service and controller for task creation

--- src/main/java/br/com/alura/AluraFake/task/TaskController.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskController.java
@@ -1,24 +1,37 @@
 package br.com.alura.AluraFake.task;
 
+import br.com.alura.AluraFake.task.dto.MultipleChoiceTaskRequest;
+import br.com.alura.AluraFake.task.dto.OpenTextTaskRequest;
+import br.com.alura.AluraFake.task.dto.SingleChoiceTaskRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/task/new")
+@RequiredArgsConstructor
 public class TaskController {
 
-    @PostMapping("/task/new/opentext")
-    public ResponseEntity newOpenTextExercise() {
-        return ResponseEntity.ok().build();
+    private final TaskService taskService;
+
+    @PostMapping("/opentext")
+    public ResponseEntity newOpenTextExercise(@RequestBody @Valid OpenTextTaskRequest request) {
+        taskService.createOpenTextTask(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PostMapping("/task/new/singlechoice")
-    public ResponseEntity newSingleChoice() {
-        return ResponseEntity.ok().build();
+    @PostMapping("/singlechoice")
+    public ResponseEntity newSingleChoice(@RequestBody @Valid SingleChoiceTaskRequest request) {
+        taskService.createSingleChoiceTask(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PostMapping("/task/new/multiplechoice")
-    public ResponseEntity newMultipleChoice() {
-        return ResponseEntity.ok().build();
+    @PostMapping("/multiplechoice")
+    public ResponseEntity newMultipleChoice(@RequestBody @Valid MultipleChoiceTaskRequest request) {
+        taskService.createMultipleChoiceTask(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
